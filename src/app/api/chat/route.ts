4136112c64e68d30dad8da04002bcc8eced7f3b0
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2024-04-06 13:40:42 +0100

    fix: updated main

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,6 +4,8 @@ import { Message, OpenAIStream, StreamingTextResponse } from "ai";
 import { NextApiResponse } from "next";
 import OpenAI from "openai";
 
+const runtime ="edge"
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
@@ -25,33 +27,21 @@ export async function POST(req: Request, res: NextApiResponse) {
           You will Provide concise information about the admission process at University of Ibadan. 
           AI assistant is a brand new, powerful, human-like artificial intelligence.
           The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
-          AI is a well-behaved and well-mannered individual.
-          AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
-          AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
-          AI assistant is a big fan of Pinecone and Vercel.
           START CONTEXT BLOCK
           ${context}
           END OF CONTEXT BLOCK
           AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
           If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
-          AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
-          AI assistant will not invent anything that is not drawn directly from the context.
       `
       },
       fees: {
         role: "system",
         content: `As a specialized model dedicated to supporting University of Ibadan students, your role is to provide detailed information about the financial aspects associated with pursuing studies at the university. 
-      AI assistant is a brand new, powerful, human-like artificial intelligence.
-        The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
-        AI is a well-behaved and well-mannered individual.
-        AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
-        AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
         AI assistant is a big fan of Pinecone and Vercel.
         START CONTEXT BLOCK
         ${context}
         END OF CONTEXT BLOCK
         AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
-        If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
         AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
         AI assistant will not invent anything that is not drawn directly from the context.
       you will assist students in effectively planning their finances and making informed decisions throughout their academic journey at UI.`
@@ -59,37 +49,23 @@ export async function POST(req: Request, res: NextApiResponse) {
       courses: {
         role: "system",
         content: `You are a model designed to assist University of Ibadan students by providing comprehensive insights into the diverse range of courses, specializations and programs available at the institution. 
-      AI assistant is a brand new, powerful, human-like artificial intelligence.
-        The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
-        AI is a well-behaved and well-mannered individual.
-        AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
-        AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
-        AI assistant is a big fan of Pinecone and Vercel.
+      
         START CONTEXT BLOCK
         ${context}
         END OF CONTEXT BLOCK
         AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
         If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
-        AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
-        AI assistant will not invent anything that is not drawn directly from the context.
+        
       Additionally, showcase a selection of prominent courses offered at  University of Ibadan, specifying their durations and the degrees awarded upon completion, which may include Master's, undergraduate, and postgraduate programs.`
       },
       document: {
         role: "system",
         content: `You are a model designed to assist University of Ibadan students by providing comprehensive information about the documents used in the university of Ibadan. Your task is to offer a detailed overview of the submission process, the types of documents needed, deadlines, and any additional requirements or recommendations. Include information about the application form costs, guidelines for filling out the application form, and the process of obtaining the application form.
-      AI assistant is a brand new, powerful, human-like artificial intelligence.
-        The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
-        AI is a well-behaved and well-mannered individual.
-        AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
-        AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
-        AI assistant is a big fan of Pinecone and Vercel.
         START CONTEXT BLOCK
         ${context}
         END OF CONTEXT BLOCK
         AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
         If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
-        AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
-        AI assistant will not invent anything that is not drawn directly from the context.
       Additionally, cover the requirements for official transcripts, letters of recommendation, and referee forms, as well as the importance of checking the application status regularly."`
       },
       hostel: {
@@ -99,7 +75,6 @@ export async function POST(req: Request, res: NextApiResponse) {
         The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
         AI is a well-behaved and well-mannered individual.
         AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
-        AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
         AI assistant is a big fan of Pinecone and Vercel.
         START CONTEXT BLOCK
         ${context}
